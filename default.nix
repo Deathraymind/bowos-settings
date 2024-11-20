34316e7a75e9bb26da1a744c97b4134f4f1d6b54
Deathraymind <deathraymind@gmail.com>
2024-11-20 17:17:52 +0900

    Update README.md

--- default.nix
+++ default.nix
@@ -1,3 +1,47 @@
-{ pkgs ? import <nixpkgs> {} }:
+{ pkgs ? import (fetchTarball {
+  url = "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz";
+  sha256 = "0sr45csfh2ff8w7jpnkkgl22aa89sza4jlhs6wq0368dpmklsl8g";
+}) {} }:
 
-pkgs.callPackage ./package.nix {}
+
+let
+  rustPlatform = pkgs.rustPlatform;
+  lib = pkgs.lib;
+in
+rustPlatform.buildRustPackage rec {
+  pname = "bowos-settings";
+  version = "v2.0.0";
+
+  # Point to the directory containing Cargo.toml and Cargo.lock
+
+  src = fetchFromGitHub {
+    owner = "deathraymind";
+    repo = "bowos-settings";
+    rev = version;
+    hash = "";
+    fetchSubmodules = true;
+  };
+  
+
+  # Use a placeholder for cargoSha256
+  cargoSha256 = "1MtDUhSQXCI8VDBLfckxLqoIuK9b6wu+HkaHwRGCbZk=";  
+
+  # Native build inputs
+  nativeBuildInputs = [
+    pkgs.nodejs
+    pkgs.pnpm
+    pkgs.wrapGAppsHook
+    pkgs.pkg-config
+    pkgs.esbuild 
+  ];
+
+  # Runtime dependencies
+  buildInputs = [
+    pkgs.gtk3
+    pkgs.libsoup
+    pkgs.openssl
+    pkgs.xdotool
+    pkgs.libayatana-appindicator
+    pkgs.webkitgtk_4_1.dev 
+  ];
+}
--- flake.nix
+++ flake.nix
@@ -1,73 +1,74 @@
 {
+  description = "Flake for bowos-settings development environment";
+
   inputs = {
-    nixpkgs.url = "nixpkgs";
-    flake-utils.url = "github:numtide/flake-utils";
+    # Fetch Nixpkgs unstable for the latest packages
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system: 
-    let
-      pkgs = nixpkgs.legacyPackages.${system};
-      
-      libraries = with pkgs; [ 
-        webkitgtk 
-        gtk3 
-        cairo 
-        gdk-pixbuf 
-        glib 
-        dbus 
-        openssl_3_2 
-        librsvg 
-        webkitgtk_4_1.dev 
-      ];
-      
-      nativeBuildInputs = with pkgs; [ 
-        pkg-config 
-        curl 
-        wget 
-        nodejs_20
-        nodePackages.npm
-        gcc 
-        rustc 
-        cargo 
-      ];
-      
-      buildInputs = libraries ++ (with pkgs; [ 
-        dbus 
-        openssl_3_2 
-        gtk3 
-        libsoup 
-        webkitgtk 
-        librsvg 
-      ]);
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+    
+    lib = pkgs.lib;
+  in
+  {
+    # Define the package
+    packages.${system}.bowos-settings = pkgs.rustPlatform.buildRustPackage rec {
+      pname = "bowos-settings";
+      version = "v2.0.0";
 
-    in {
-      devShell = pkgs.mkShell {
-        inherit buildInputs nativeBuildInputs;
-        
-        shellHook = ''
-          export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
-        '';
-      };
-      
-      packages.default = pkgs.rustPlatform.buildRustPackage {
-        pname = "bowos-settings";
-        version = "0.1.0";
-        src = pkgs.lib.cleanSource ./bowos-settings/src-tauri;
-        cargoLock = {
-          lockFile = ./bowos-settings/src-tauri/Cargo.lock;
-          allowBuiltinFetchGit = true;
-        };
-        
-        inherit buildInputs nativeBuildInputs;
-        
-        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-        
-        
-        postInstall = ''
-          wrapProgram $out/bin/bowos-settings \
-            --set LD_LIBRARY_PATH ${pkgs.lib.makeLibraryPath libraries}
-        '';
-      };
-    }
-  );
+      src = ./bowos-settings/src-tauri;
+
+      cargoSha256 = "1MtDUhSQXCI8VDBLfckxLqoIuK9b6wu+HkaHwRGCbZk=";  
+
+      nativeBuildInputs = [
+        pkgs.nodejs
+        pkgs.pnpm
+        pkgs.wrapGAppsHook
+        pkgs.pkg-config
+        pkgs.esbuild 
+      ];
+
+      buildInputs = [
+        pkgs.gtk3
+        pkgs.libsoup
+        pkgs.openssl
+        pkgs.xdotool
+        pkgs.libayatana-appindicator
+        pkgs.webkitgtk_4_1.dev 
+      ];
+    };
+
+    # Define the development shell
+    devShell.${system} = pkgs.mkShell {
+      nativeBuildInputs = [
+        pkgs.nodejs
+        pkgs.pnpm
+        pkgs.wrapGAppsHook
+        pkgs.pkg-config
+        pkgs.esbuild
+        pkgs.rustPlatform.rustc
+        pkgs.rustPlatform.cargo
+      ];
+
+      buildInputs = [
+        pkgs.gtk3
+        pkgs.libsoup
+        pkgs.openssl
+        pkgs.xdotool
+        pkgs.libayatana-appindicator
+        pkgs.webkitgtk_4_1.dev
+      ];
+
+      # Environment variables (optional)
+      shellHook = ''
+        echo "Entering development environment for bowos-settings"
+      '';
+    };
+  };
 }
